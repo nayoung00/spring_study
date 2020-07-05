5155b3f5972db2227ccb857023f52ab0ac4b5164
nayoung00 <invin1201@gmail.com>
2020-07-06 00:14:04 +0900

    day03_class3.model2 로그인 기능 구현하기

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.springbook.biz.day02.user.UserVO;
+import com.springbook.biz.day02.user.impl.UserDAO;
 
 public class DispatcherServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -33,6 +35,25 @@ public class DispatcherServlet extends HttpServlet {
     // 2. 클라이언트의 요청 path에 따라 적절히 분기처리 한다.
     if (path.equals("/login.do")) {
       System.out.println("로그인 처리");
+
+      // 1. 사용자 입력 정보 추출
+      String id = request.getParameter("id");
+      String password = request.getParameter("password");
+
+      // 2. DB연동 처리
+      UserVO vo = new UserVO();
+      vo.setId(id);
+      vo.setPassword(password);
+
+      UserDAO userDAO = new UserDAO();
+      UserVO user = userDAO.getUser(vo);
+
+      // 3. 화면 네비게이션
+      if (user != null) {
+        response.sendRedirect("getBoardList.jsp");
+      } else {
+        response.sendRedirect("login.jsp");
+      }
     } else if (path.equals("/logout.do")) {
       System.out.println("로그아웃 처리");
     } else if (path.equals("/insertBoard.do")) {
