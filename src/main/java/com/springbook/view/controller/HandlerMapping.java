0d7cd9b0368fed62c90d609cc5a0fd7255eb6d27
nayoung00 <invin1201@gmail.com>
2020-07-07 00:04:24 +0900

    day03_class4.HandlerMapping 구현

---
+++ src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -0,0 +1,20 @@
+package com.springbook.view.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 모든 Controller 객체들을 저장하고 있다가,
+// 클라이언트의 요청을 처리할 특정 Controller를 검색하는 기능을 제공한다.
+public class HandlerMapping {
+  private Map<String, Controller> mappings;
+
+  public HandlerMapping() {
+    mappings = new HashMap<String, Controller>();
+    mappings.put("/login.do", new LoginController());
+  }
+
+  public Controller getController(String path) {
+    return mappings.get(path);
+  }
+
+}
