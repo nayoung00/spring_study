ed8b2016201138cd19284a3138abecb7d0fc3282
nayoung00 <invin1201@gmail.com>
2020-07-06 22:47:03 +0900

    day03_class3.model2 글수정 기능 구현하기

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -84,6 +84,21 @@ public class DispatcherServlet extends HttpServlet {
 
     } else if (path.equals("/updateBoard.do")) {
       System.out.println("글 수정 처리");
+
+      // 1. 사용자 입력 정보 추출
+      // request.setCharacterEncoding("EUC-KR");
+      String title = request.getParameter("title");
+      String content = request.getParameter("content");
+      String seq = request.getParameter("seq");
+
+      // 2. DB 연동 처리
+      BoardVO vo = new BoardVO();
+      vo.setTitle(title);
+      vo.setContent(content);
+      vo.setSeq(Integer.parseInt(seq));
+
+      BoardDAO boardDAO = new BoardDAO();
+
     } else if (path.equals("/deleteBoard.do")) {
       System.out.println("글 삭제 처리");
     } else if (path.equals("/getBoard.do")) {
