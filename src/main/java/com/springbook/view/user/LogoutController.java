ffe5e8f2970efebaab86edab4bc2799a51f99425
nayoung00 <invin1201@gmail.com>
2020-07-08 19:18:09 +0900

    day03_class6-7.Spring MVC적용_로그아웃 기능 구현하기

--- src/main/java/com/springbook/view/user/LogoutController.java
+++ src/main/java/com/springbook/view/user/LogoutController.java
@@ -3,12 +3,13 @@ package com.springbook.view.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import com.springbook.view.controller.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 public class LogoutController implements Controller {
 
   @Override
-  public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 
     System.out.println("로그아웃 처리");
 
@@ -17,7 +18,9 @@ public class LogoutController implements Controller {
     session.invalidate();
 
     // 2. 세션 종료 후, 메인 화면으로 이동한다.
-    return "login";
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("login.jsp");
+    return mav;
   }
 
 
