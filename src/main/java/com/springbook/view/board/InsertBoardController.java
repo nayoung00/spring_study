220f9ef8f77118cd6c9c6ef83913b0a95dd5309f
nayoung00 <invin1201@gmail.com>
2020-07-08 22:57:29 +0900

    day04_class02.어노테이션으로 게시판 프로그램 구현하기
    2.1 글 등록 기능 구현하기

--- src/main/java/com/springbook/view/board/InsertBoardController.java
+++ src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,6 +1,5 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.springbook.biz.day02.board.BoardVO;
@@ -10,23 +9,9 @@ import com.springbook.biz.day02.board.impl.BoardDAO;
 public class InsertBoardController {
 
   @RequestMapping(value = "/insertBoard.do")
-  public void insertBoard(HttpServletRequest request) {
+  public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
     System.out.println("글 등록 처리");
-
-    // 1. 사용자 입력 정보 추출
-    // request.setCharacterEncoding("UTF-8");
-    String title = request.getParameter("title");
-    String writer = request.getParameter("writer");
-    String content = request.getParameter("content");
-
-    // 2. DB 연동 처리
-    BoardVO vo = new BoardVO();
-    vo.setTitle(title);
-    vo.setWriter(writer);
-    vo.setContent(content);
-
-    BoardDAO boardDAO = new BoardDAO();
     boardDAO.insertBoard(vo);
-
+    return "getBoardList.do";
   }
 }
