bdc321bc279104d51f8b5ff20fda510796f13aad
nayoung00 <invin1201@gmail.com>
2020-07-08 19:14:01 +0900

    day03_class6-7.Spring MVC적용_글 등록 기능 구현하기

--- src/main/java/com/springbook/view/board/InsertBoardController.java
+++ src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -2,14 +2,15 @@ package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 import com.springbook.biz.day02.board.BoardVO;
 import com.springbook.biz.day02.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
-public class InsertController implements Controller {
+public class InsertBoardController implements Controller {
 
   @Override
-  public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 
     System.out.println("글 등록 처리");
 
@@ -29,7 +30,9 @@ public class InsertController implements Controller {
     boardDAO.insertBoard(vo);
 
     // 3. 화면 네비게이션
-    return "getBoardList.do";
+    ModelAndView mav = new ModelAndView();
+    mav.setViewName("getBoardList.do");
+    return mav;
   }
 
 }
