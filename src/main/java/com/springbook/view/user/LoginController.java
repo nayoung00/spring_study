0d5367b67280b79cd00780d030c6a617be5791af
nayoung00 <invin1201@gmail.com>
2020-07-09 13:07:08 +0900

    day04_class02.어노테이션으로 게시판 프로그램 구현하기
    2.9 요청 방식에 따른 처리

--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -1,20 +1,29 @@
 package com.springbook.view.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.springbook.biz.day02.user.UserVO;
 import com.springbook.biz.day02.user.impl.UserDAO;
 
 @Controller
 public class LoginController {
 
-  @RequestMapping("/login.do")
+  @RequestMapping(value = "/login.do", method = RequestMethod.GET)
+  public String loginView(@ModelAttribute("user") UserVO vo) {
+    System.out.println("로그인 화면으로 이동");
+    vo.setId("test");
+    vo.setPassword("test123");
+    return "login.jsp";
+  }
+
+  @RequestMapping(value = "/login.do", method = RequestMethod.POST)
   public String login(UserVO vo, UserDAO userDAO) {
+    System.out.println("로그인 인증 처리...");
     if (UserDAO.getUser(vo) != null)
       return "getBoardList.do";
     else
       return "login.jsp";
-
   }
-
 }
