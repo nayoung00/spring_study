3376cffd635d1dd561f925a484685cbc241237cd
nayoung00 <invin1201@gmail.com>
2020-07-13 22:14:22 +0900

    day05_class01.Mybatis프레임워크 시작하기

--- src/main/java/com/springbook/biz/day02/common/AfterAdvice.java
+++
@@ -1,5 +0,0 @@
-package com.springbook.biz.day02.common;
-
-public class AfterAdvice {
-
-}
--- src/main/java/com/springbook/biz/day02/common/Log4jAdvice.java
+++
@@ -1,7 +0,0 @@
-package com.springbook.biz.day02.common;
-
-public class Log4jAdvice {
-  public void printLogging() {
-    System.out.println("[공통 로그 - Logj4] 비즈니스 로직 수행 전 동작");
-  }
-}
